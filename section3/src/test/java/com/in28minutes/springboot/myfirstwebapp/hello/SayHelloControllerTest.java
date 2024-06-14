9d21ee1b93f0b287a7e864c9052567dce546ca32
BogdanSicu <b0gdan.munt3@gmail.com>
2024-06-14 13:40:54 +0300

    added section 7

---
+++ section3/src/test/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloControllerTest.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SayHelloControllerTest {
+
+    @Test
+    void sayHello() {
+    }
+
+    @Test
+    void sayHelloHtml() {
+    }
+
+    @Test
+    void sayHelloJsp() {
+    }
+}
---
+++ section7/build.gradle.kts
@@ -0,0 +1,36 @@
+import java.sql.DriverManager.println
+
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.0"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.munte"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(22)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.register("helloWorld") {
+	doLast {
+		println("Hello World");
+	}
+}
