b3f47eb030bf506a546fc7f8c0aac7101fae38e2
BogdanSicu <b0gdan.munt3@gmail.com>
2024-06-17 12:50:06 +0300

    start of section 8 - rest api

---
+++ section8-rest-api/src/main/java/com/munte/section8_rest_api/Section8RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.munte.section8_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section8RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section8RestApiApplication.class, args);
+	}
+
+}
---
+++ section8-rest-api/src/test/java/com/munte/section8_rest_api/Section8RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munte.section8_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section8RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
